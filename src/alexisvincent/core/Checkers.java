1bd9a292c038cb96cb59744b0d50f7814beb671b
Alexis Vincent <alexisjohnvincent@gmail.com>
2015-03-10 21:45:18 +0200

    All Setup and Shit... Free the penguins

--- src/alexisvincent/core/Checkers.java
+++ src/alexisvincent/core/Checkers.java
@@ -1,7 +1,61 @@
 package alexisvincent.core;
 
+import applicationFramework.core.Application;
+import applicationFramework.ui.GUI;
+import swing.components.AColor;
+import swing.components.ASwopPane;
+
+import javax.swing.*;
+import java.awt.*;
+
+
 /**
- * Created by alexisvincent on 15/03/10.
+ * Main Application
  */
-public class Checkers {
+public class Checkers extends Application {
+
+    static {
+        INSTANCE = new Checkers();
+    }
+
+    private Checkers() {
+        super();
+    }
+
+    public static Application getINSTANCE() {
+        return INSTANCE;
+    }
+
+    public static void main(String[] args) {
+        getINSTANCE();
+    }
+
+    @Override
+    protected void initGUI() {
+        //Init Gui
+        SwingUtilities.invokeLater(new Runnable() {
+
+            @Override
+            public void run() {
+                gui = new GUI("Checkers").setDimensions(new Dimension(900, 600)).setGoodDefaults();
+                gui.getFrame().setContentPane(new ApplicationPane());
+                gui.show();
+            }
+        });
+    }
+
+    /**
+     * Application GUI Containing A Menu and the Sorting Image
+     */
+    private class ApplicationPane extends ASwopPane {
+
+        public ApplicationPane() {
+            super();
+            setName("ApplicationPane");
+            setBackground(AColor.DarkGreen);
+
+            setLayout(new GridBagLayout());
+
+        }
+    }
 }
